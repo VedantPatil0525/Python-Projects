3a7c68f56dbd6e213098c3df462671c16609ce3c
Vedant Patil <vedpatil1432@gmail.com>
2025-12-20 20:55:08 +0530

    Implementing python basics - advanced

---
+++ Simple_Calculator.py
@@ -0,0 +1,37 @@
+def add(x, y):
+  return x + y
+
+def substract(x, y):
+  return x - y
+
+def multiply(x, y):
+  return x * y
+
+def divide(x, y):
+  if y == 0:
+    return "Error! Cannot divide by zero."
+  return x / y
+
+def power(x, y):
+  return x**y
+
+print("Select Operation:")
+print("1. Add\n2. Substract\n3. Multiply\n4. Divide\n5. Power")
+
+choice = input("Enter choice (1/2/3/4/5):")
+
+num1 = float(input("Enter first number:"))
+num2 = float(input("Enter second number:"))
+
+if choice == '1':
+  print("Result:", add(num1, num2))
+elif choice == '2':
+  print("Result:", substract(num1, num2))
+elif choice == '3':
+  print("Result:", multiply(num1, num2))
+elif choice == '4':
+  print("Result:", divide(num1, num2))
+elif choice == '5':
+  print("Result:", power(num1, num2))
+else:
+  print("Invalid choice")
